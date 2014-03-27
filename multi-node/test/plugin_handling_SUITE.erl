e6e8885524a8aa198f6b72ce2f80519e5442777b
Tim Watson <tim@rabbitmq.com>
2014-03-27 11:45:11 +0000

    Groups tests, minimise setup times and shuffle/randomise execution order

--- multi-node/test/plugin_handling_SUITE.erl
+++ multi-node/test/plugin_handling_SUITE.erl
@@ -18,15 +18,30 @@
 -include_lib("common_test/include/ct.hrl").
 -include_lib("systest/include/systest.hrl").
 
--export([suite/0, all/0, init_per_suite/1, end_per_suite/1,
+-export([suite/0, all/0, groups/0]).
+-export([init_per_suite/1, end_per_suite/1,
+         init_per_group/2, end_per_group/2,
          init_per_testcase/2, end_per_testcase/2]).
 -export([basic_enable/1, transitive_dependency_handling/1,
-         offline_must_be_explicit/1]).
+         offline_must_be_explicit/1, management_extension_handling/1]).
+
+%% Configuration
 
 suite() -> [{timetrap, systest:settings("time_traps.ha_cluster_SUITE")}].
 
 all() ->
-    systest_suite:export_all(?MODULE).
+    [{group, with_running_nodes},
+     management_extension_handling,
+     {group, with_offline_nodes}].
+
+groups() ->
+    [{with_running_nodes, [shuffle],
+      [basic_enable,
+       transitive_dependency_handling]},
+     {with_offline_nodes, [shuffle],
+      [offline_must_be_explicit]}].
+
+%% Setup/Teardown
 
 init_per_suite(Config) ->
     Config.
@@ -34,12 +49,43 @@ init_per_suite(Config) ->
 end_per_suite(_Config) ->
     ok.
 
-init_per_testcase(_TC, Config) ->
-    file:delete(enabled_plugins_file()),
+init_per_group(with_offline_nodes=Group, Config) ->
+    [rabbit_ha_test_utils:stop_app(Node) ||
+        {Node, _} <- systest:list_processes(Group)],
+    Config;
+init_per_group(_Group, Config) ->
     Config.
 
+end_per_group(_Group, Config) ->
+    {save_config, Config}.
+
+init_per_testcase(TC, Config) ->
+    file:delete(enabled_plugins_file()),
+    [{Node1, _}, {Node2, _}] = systest:list_processes(plugin_handling),
+    Config2 = case TC of
+                  management_extension_handling ->
+                      rabbit_ha_test_utils:stop_app(Node1),
+                      [{stopped, Node1}];
+                  _ ->
+                      []
+              end,
+    [{node1, Node1}, {node2, Node2}] ++ Config2 ++ Config.
+
+%% TODO: consider putting mgmt_extension_handling into another group
+
+end_per_testcase(management_extension_handling, _Config) ->
+    [{Node1, _}|_] = systest:list_processes(plugin_handling),
+    try
+        %% this *can* fail in the testcase, if not it'll fail here
+        %% and we want to remain as oblivious as we possibly can
+        rabbit_ha_test_utils:start_app(Node1),
+        ok
+    catch _:_ -> ok
+    end;
 end_per_testcase(_, _) ->
     ok.
+
+%% Test Cases
 
 basic_enable(Config) ->
     SUT = systest:active_sut(Config),
@@ -64,14 +110,28 @@ transitive_dependency_handling(Config) ->
 
 offline_must_be_explicit(Config) ->
     SUT = systest:active_sut(Config),
-    [{A, ARef},
+    [{A, _ARef},
      {_B, _BRef}] = systest:list_processes(SUT),
-    systest:stop_and_wait(ARef),
+    rabbit_ha_test_utils:stop_app(A),
     try
         enable(rabbitmq_shovel, A),
         exit({node_offline, "without --offline, rabbitmq-plugins should crash"})
     catch _:{error_string, _} -> ok
+    after
+        rabbit_ha_test_utils:start_app(A)
     end.
+
+management_extension_handling(Config) ->
+    SUT = systest:active_sut(Config),
+    [{C, _ARef},
+     {_B, _BRef}] = systest:list_processes(SUT),
+    enable_offline(rabbitmq_management, C),
+    enable_offline(rabbitmq_shovel_management, C),
+    rabbit_ha_test_utils:start_app(C),
+    verify_app_running(rabbitmq_shovel_management, C),
+    disable(rabbitmq_shovel_management, C),
+    verify_app_not_running(rabbitmq_shovel_management, C),
+    ok.
 
 verify_app_running(App, Node) ->
     {App, _, _} = lists:keyfind(App, 1, get_running_apps(Node)).
