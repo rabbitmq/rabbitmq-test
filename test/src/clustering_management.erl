97cd6ba436a026a264e7b9de9eb5a11b4bed012f
Simon MacMullen <simon@rabbitmq.com>
2014-09-08 14:25:00 +0100

    Add a test.

--- test/src/clustering_management.erl
+++ test/src/clustering_management.erl
@@ -340,6 +340,17 @@ erlang_config(Config) ->
     assert_cluster_status({[Rabbit, Hare], [Rabbit], [Rabbit, Hare]},
                           [Rabbit, Hare]),
 
+    %% Check having a stop_app'ed node around doesn't break completely.
+    ok = stop_app(Hare),
+    ok = reset(Hare),
+    ok = stop_app(Rabbit),
+    ok = rpc:call(Hare, application, set_env,
+                  [rabbit, cluster_nodes, {[Rabbit], disc}]),
+    ok = start_app(Hare),
+    ok = start_app(Rabbit),
+    assert_not_clustered(Hare),
+    assert_not_clustered(Rabbit),
+
     %% We get a warning but we start anyway
     ok = stop_app(Hare),
     ok = reset(Hare),
