e19289c28c590ee2090f3aac5c931bcf85477778
Michael Klishin <mklishin@pivotal.io>
2016-02-22 19:29:37 +0300

    Wording

--- test/src/rabbit_plugins_tests.erl
+++ test/src/rabbit_plugins_tests.erl
@@ -4,19 +4,19 @@
 
 test_version_support() ->
     Examples = [
-     {[], "any version", ok} % any version
-    ,{[], "0.0.0", ok}       % any version
-    ,{[], "3.5.6", ok}       % any version
-    ,{["something"], "something", ok}      % Equal always match 
-    ,{["3.5.4"], "something", err}
-    ,{["something", "3.5.6"], "3.5.7", ok} % any version can match
-    ,{["3.4.0", "3.5.6"], "3.6.1", err}    % major version should present
-    ,{["3.5.2", "3.6.1"], "3.5.2", ok}     % equal version match
-    ,{["3.5.2", "3.6.1"], "3.5.1", err}    % lesser version don't match
-    ,{["3.5.2", "3.6.1"], "3.6.2", ok}     % greated version match
-    ,{["3.5.2", "3.6.1"], "0.0.0", err}    % default version don't match!
-    ,{["3.5", "3.6"], "3.5.1", err}        % Shorter version not supported
-    ,{["3"], "3.5.1", err}                 
+     {[], "any version", ok} %% anything goes
+    ,{[], "0.0.0", ok}       %% ditto
+    ,{[], "3.5.6", ok}       %% ditto
+    ,{["something"], "something", ok}      %% equal values match
+    ,{["3.5.4"], "something", err}         %% mismatching value
+    ,{["something", "3.5.6"], "3.5.7", ok} %% ???
+    ,{["3.4.0", "3.5.6"], "3.6.1", err}    %% greater than the range
+    ,{["3.5.2", "3.6.1"], "3.5.2", ok}     %% lower boundary matches
+    ,{["3.5.2", "3.6.1"], "3.5.1", err}    %% lesser than the range
+    ,{["3.5.2", "3.6.1"], "3.6.2", ok}     %% ???
+    ,{["3.5.2", "3.6.1"], "0.0.0", err}    %% not in the range
+    ,{["3.5", "3.6"], "3.5.1", err}        %% x.y values are not supported
+    ,{["3"], "3.5.1", err}                 %% x values are not supported
     ],
 
     lists:foreach(
@@ -25,8 +25,8 @@ test_version_support() ->
                 ok  -> ok;
                 err -> {error, {version_mismatch, {RabbitVersion, Versions}}}
             end,
-            {Expected, RabbitVersion, Versions} = 
-                {rabbit_plugins:version_support(RabbitVersion, Versions), 
+            {Expected, RabbitVersion, Versions} =
+                {rabbit_plugins:version_support(RabbitVersion, Versions),
                  RabbitVersion, Versions}
         end,
         Examples),
