f1aa0306f0a99ee762958c135fd6978c81cb64cd
Daniil Fedotov <dfedotov@pivotal.io>
2016-04-08 14:34:01 +0100

    Version "0.0.0" and "" always match

--- test/src/rabbit_plugin_versioning_tests.erl
+++ test/src/rabbit_plugin_versioning_tests.erl
@@ -1,37 +1,39 @@
 -module(rabbit_plugin_versioning_tests).
 -include("rabbit.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 -compile(export_all).
 
-test_version_support() ->
+version_support_test() ->
     Examples = [
      {[], "any version", true} %% anything goes
     ,{[], "0.0.0", true}       %% ditto
     ,{[], "3.5.6", true}       %% ditto
     ,{["something"], "something", true}            %% equal values match
     ,{["3.5.4"], "something", false}
+    ,{["3.4.5", "3.6.0"], "0.0.0", true}           %% zero version always match
+    ,{["3.4.5", "3.6.0"], "", true}                %% empty version always match
     ,{["something", "3.5.6"], "3.5.7", true}       %% 3.5.7 matches ~> 3.5.6
     ,{["3.4.0", "3.5.6"], "3.6.1", false}          %% 3.6.x isn't supported
     ,{["3.5.2", "3.6.1", "3.7.1"], "3.5.2", true}  %% 3.5.2 matches ~> 3.5.2
     ,{["3.5.2", "3.6.1", "3.7.1"], "3.5.1", false} %% lesser than the lower boundary
     ,{["3.5.2", "3.6.1", "3.7.1"], "3.6.2", true}  %% 3.6.2 matches ~> 3.6.1
-    ,{["3.5.2", "3.6.1", "3.7.1"], "0.0.0", false}
     ,{["3.5", "3.6", "3.7"], "3.5.1", false}       %% x.y values are not supported
     ,{["3"], "3.5.1", false}                       %% x values are not supported
     ],
 
     lists:foreach(
-        fun({Versions, RabbitVersion, _Result}) ->
-            {_Expected, RabbitVersion, Versions} =
+        fun({Versions, RabbitVersion, Expected}) ->
+            {Expected, RabbitVersion, Versions} =
                 {rabbit_plugins:is_version_supported(RabbitVersion, Versions),
                  RabbitVersion, Versions}
         end,
         Examples),
-    passed.
+    ok.
 
 -record(validation_example, {rabbit_version, plugins, errors, valid}).
 
-test_plugin_validation() ->
+plugin_validation_test() ->
     Examples = [
         #validation_example{
          rabbit_version = "3.7.1",
@@ -72,9 +74,17 @@ test_plugin_validation() ->
            {plugin_b, "3.7.2", ["3.7.0"], [{plugin_a, ["3.7.3"]}]},
            {plugin_d, "3.7.2", ["3.7.0"], [{plugin_c, ["3.7.3"]}]}],
          errors =
-          [{plugin_b, [{{version_mismatch, "3.7.1", ["3.7.3"]}, plugin_a}]},
+          [{plugin_b, [{{dependency_version_mismatch, "3.7.1", ["3.7.3"]}, plugin_a}]},
            {plugin_d, [{missing_dependency, plugin_c}]}],
          valid = [plugin_a]
+        },
+        #validation_example{
+         rabbit_version = "0.0.0",
+         plugins =
+          [{plugin_a, "", ["3.7.1"], []},
+           {plugin_b, "3.7.2", ["3.7.0"], [{plugin_a, ["3.7.3"]}]}],
+         errors = [],
+         valid  = [plugin_a, plugin_b]
         }],
     lists:foreach(
         fun(#validation_example{rabbit_version = RabbitVersion,
@@ -91,7 +101,7 @@ test_plugin_validation() ->
                                                     Valid))
         end,
         Examples),
-    passed.
+    ok.
 
 make_plugins(Plugins) ->
     lists:map(
