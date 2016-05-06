c7d10865f37304aefb6c21cc0bf4e3dbe4e16e16
Daniil Fedotov <dfedotov@pivotal.io>
2016-05-06 11:08:11 +0100

    Support for plugin channels in channel test

--- test/src/channel_interceptor_test.erl
+++ test/src/channel_interceptor_test.erl
@@ -6,36 +6,39 @@
 
 register_interceptor_test() ->
     {ok, C} = amqp_connection:start(#amqp_params_network{}),
+
+    PredefinedChannels = rabbit_channel:list(),
+
     {ok, Ch1} = amqp_connection:open_channel(C),
 
     amqp_channel:call(Ch1, #'queue.declare'{queue = <<"foo">>}),
 
-    [ChannelProc] = rabbit_channel:list(),
+    [ChannelProc] = rabbit_channel:list() -- PredefinedChannels,
 
     [{interceptors, []}] = rabbit_channel:info(ChannelProc, [interceptors]),
-    
+
     check_send_receive(Ch1, <<"bar">>, <<"bar">>),
-    
-    ok = rabbit_registry:register(channel_interceptor, 
-                                  <<"stub interceptor">>, 
+
+    ok = rabbit_registry:register(channel_interceptor,
+                                  <<"stub interceptor">>,
                                   stub_interceptor),
     [{interceptors, [{stub_interceptor, undefined}]}] = rabbit_channel:info(ChannelProc, [interceptors]),
 
     check_send_receive(Ch1, <<"bar">>, <<"">>),
 
-    ok = rabbit_registry:unregister(channel_interceptor, 
+    ok = rabbit_registry:unregister(channel_interceptor,
                                   <<"stub interceptor">>),
     [{interceptors, []}] = rabbit_channel:info(ChannelProc, [interceptors]),
-    
+
     check_send_receive(Ch1, <<"bar">>, <<"bar">>),
     passed.
 
 
 check_send_receive(Ch1,  Send, Receive) ->
-    amqp_channel:call(Ch1, 
-                        #'basic.publish'{routing_key = <<"foo">>}, 
+    amqp_channel:call(Ch1,
+                        #'basic.publish'{routing_key = <<"foo">>},
                         #amqp_msg{payload = Send}),
 
-    {#'basic.get_ok'{}, #amqp_msg{payload = Receive}} = 
-        amqp_channel:call(Ch1, #'basic.get'{queue = <<"foo">>, 
+    {#'basic.get_ok'{}, #amqp_msg{payload = Receive}} =
+        amqp_channel:call(Ch1, #'basic.get'{queue = <<"foo">>,
                                               no_ack = true}).
